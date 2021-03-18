ac9de0651fde27f38a9fea5312daf342a50d3c22
AlifiantoNarendra <alifiantonarendra@gmail.com>
2021-03-18 15:40:07 +0700

    Instal PM2

--- app.js
+++ app.js
@@ -21,8 +21,39 @@ import sequelize from "sequelize";
 import swaggerUi from "swagger-ui-express";
 import swaggerJsDoc from "swagger-jsdoc";
 
+// // pm2
+// import pm2 from "pm2";
+
+// pm2.connect(function (err) {
+//    if (err) {
+//       console.error(err);
+//       process.exit(2);
+//    }
+
+//    pm2.list((err, list) => {
+//       console.log(err, list);
+//    });
+
+//    pm2.stop("app-name", (err, proc) => {});
+
+//    pm2.restart("app-name", (err, proc) => {});
+
+//    pm2.start(
+//       {
+//          script: "app.js", // Script to be run
+//          exec_mode: "cluster", // Allows your app to be clustered
+//          instances: 4, // Optional: Scales your app by 4
+//          max_memory_restart: "100M", // Optional: Restarts your app if it reaches 100Mo
+//       },
+//       function (err, apps) {
+//          pm2.disconnect(); // Disconnects from PM2
+//          if (err) throw err;
+//       }
+//    );
+// });
+
 // port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const app = express();
 
